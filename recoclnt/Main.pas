84c909aef45ab812a0c85ef1459acdd5aef6d057
GlooK <tdlite@mail.ru>
2012-02-08 01:11:51 +0400

    changed form

--- recoclnt/Main.pas
+++ recoclnt/Main.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, ExtCtrls, StdCtrls, jpeg;
 
 type
   TfrmRecoclnt = class(TForm)
+    imgHeader: TImage;
+    imgWarning: TImage;    
+    lblEntry: TStaticText;
+    lblComment: TStaticText;
+    txtComment: TMemo;
+    btnSend: TButton;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +27,12 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TfrmRecoclnt.FormCreate(Sender: TObject);
+begin
+lblEntry.Caption := 'Вход в систему был произведен после времени начала' +
+  ' рабочего дня.' + #10#13 + 'Пожалуйста, укажите поясняющий' +
+  ' комментарий для Вашего руководителя.';
+end;
 
 end.
