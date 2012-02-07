ced88763b89a15e6c64ee9e26ea0b2bcfe9eddeb
GlooK <tdlite@mail.ru>
2012-02-07 21:55:53 +0400

    added recoclnt

---
+++ recoclnt/Main.pas
@@ -0,0 +1,24 @@
+unit Main;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmRecoclnt = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmRecoclnt: TfrmRecoclnt;
+
+implementation
+
+{$R *.dfm}
+
+end.
